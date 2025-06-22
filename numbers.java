d88580ab6ff992c577dfb31f5b8946262de35d4e
root <root@ip-172-31-46-72.eu-north-1.compute.internal>
2025-06-22 16:43:19 +0000

    *

--- numbers.java
+++ numbers.java
@@ -1,12 +1,11 @@
-public class login
-{
-	public static void main()
+public class login 
+{public static void main()
 	{
-
 		int i=0;
-		for (i=0;i<==35;i+);
+		for (i=0;i<==35;i++)
 		{
-			system.out.println("Numbers: "+i);
+			system.out.println("Number: "++);
 		}
 	}
 }
+	
--- sample.java
+++ sample.java
@@ -1,3 +1,11 @@
-well come to srtech
-
-
+public class login
+{
+	public static void main()
+	{
+i`	     int i =0;
+		     for (i=0;i<==35;i++)
+	     {
+	      system.out.println("Number: "+i);
+	     }
+	}
+}	
