61eecac7d4c18fe99c90357c5c5e7241ec7c5084
root <root@ip-172-31-42-56.eu-north-1.compute.internal>
2025-06-22 12:50:08 +0000

    numbers.java code

---
+++ numbers.java
@@ -0,0 +1,11 @@
+public numbers login
+{
+        public static void main()
+        {
+                int i =0;
+                for (i=0;i<==35;i++)
+                {
+                        system.out.println("Numbers: "+i);
+                }
+        }
+}
