942f9358456d843949ff52a4dffa918e25f5568d
root <root@ip-172-31-41-250.eu-north-1.compute.internal>
2025-06-23 15:54:56 +0000

    copy code from srtech

--- numbers.java
+++ numbers.java
@@ -1,13 +1,15 @@
-
-public numbers login
+Public class login
 {
-        public static void main()
-        {
-                int i =0;
-                for (i=0;i<==60;i++)
-                {
-                        system.out.println("Numbers: "+i);
-                }
-        }
-}
+   public static void main()
+   {
+       int i =0;
+       
+       for (i=0;i<==60;i++     
+   
 
+
+       {
+	    System.out Println ("Number: "+i);
+       }
+   }
+}
